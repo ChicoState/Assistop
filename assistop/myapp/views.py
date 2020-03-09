c284dc7d7f2cea463ff7b2b9216b5b6ac655c1f8
Paul <pdhenson@mail.csuchico.edu>
2020-03-09 01:23:51 +0000

    Adding View.py to resolve conflicts

--- assistop/myapp/views.py
+++ assistop/myapp/views.py
@@ -1,7 +1,61 @@
 from django.shortcuts import render
+<<<<<<< HEAD
 from . import models
 
 # Create your views here.
 
 def home(request):
     return render(request, "home.html")
+=======
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+	context={
+		"title": "Assistop: Dashboard",
+	}
+	return render(request, "index.html", context = context)
+
+def assistants(request):
+    context={
+        "title": "Assistop: My Assistants",
+    }
+    return render(request, "assistants.html", context=context)
+
+def devices(request):
+    context={
+        "title": "Assistop: My Devices",
+    }
+    return render(request, "devices.html", context=context)
+
+def schedule(request):
+    context={
+        "title": "Assistop: Schedule",
+    }
+    return render(request, "schedule.html", context=context)
+
+def notifications(request):
+    context={
+        "title": "Assistop: Notifications",
+    }
+    return render(request, "notifications.html", context=context)
+
+def hotspot(request):
+    context={
+        "title": "Assistop: Hotspot",
+    }
+    return render(request, "hotspot.html", context=context)
+
+def system(request):
+    context={
+        "title": "Assistop: System",
+    }
+    return render(request, "system.html", context=context)
+
+def about(request):
+    context={
+        "title": "Assistop: About",
+    }
+    return render(request, "about.html", context=context)
+>>>>>>> f4861a38680530e5f1ecfa6d36997094a25cb4f4
